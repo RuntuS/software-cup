b7df6c08d09090783e0bad0e2f7237370ef40ac1
RuntuS <893119806@qq.com>
2021-07-13 21:30:41 +0800

    feat: add background-image

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,7 +5,7 @@ import { Button, Input, Menu, message } from 'antd'
 import React, { useCallback, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import url from './image/相机.svg'
-import { StyleLoginBody } from './style'
+import { StyleImageBox, StyleLoginBody } from './style'
 
 
 
@@ -48,47 +48,54 @@ export const Login: React.FC<Props> = (props) => {
     
 
     return (
-        <StyleLoginBody style={{height: window.innerHeight}}>
-            <div className="titleBox">
-                <div className="imageBox">
-                    <img src={url} alt="标志"/>
+        <>
+            <StyleImageBox style={{height: window.innerHeight}}>
+                <img src="https://oss-album.oss-cn-beijing.aliyuncs.com/userImg/beijing.jpg"  alt='背景图片' />
+            </StyleImageBox>
+            <StyleLoginBody style={{height: window.innerHeight}}>
+                <div className={'allBox'}>
+                    <div className="titleBox">
+                        <div className="imageBox">
+                            <img src={url} alt="标志"/>
+                        </div>
+                        <span className="title">智能云相册</span>
+                    </div>
+                    <div className="subTitle">
+                        新一代智能相册，释放您的硬件空间！
+                    </div>
+                    <Menu  mode="horizontal" selectedKeys={["login"]} className="menu">
+                        <Item key="login" icon={<LoginOutlined />}>
+                            账号密码登录
+                        </Item>
+                    </Menu>
+
+                    <div className="loginBox">
+                        <Input
+                            onChange={(e) => {accountInput(e.target.value)}}
+                            className={"input"}
+                            prefix={<UserOutlined className={'icon'} />}
+                            placeholder="登录账号"
+                        />
+                        <Input
+                            onChange={(e) => {passwordInput(e.target.value)}}
+                            className={"input"}
+                            prefix={<LockOutlined className={'icon'} />}
+                            placeholder="密码"
+                            type="password"
+                        />
+                    </div>
+
+                    <div className="buttonBox">
+                        <Button 
+                            type="primary" 
+                            className="loginBtn"
+                            onClick={() => {loginLocal()}}
+                        >
+                            登录
+                        </Button>
+                    </div>
                 </div>
-                <span className="title">智能云相册</span>
-            </div>
-            <div className="subTitle">
-                新一代智能相册，释放您的硬件空间！
-            </div>
-            <Menu  mode="horizontal" selectedKeys={["login"]} className="menu">
-                <Item key="login" icon={<LoginOutlined />}>
-                    账号密码登录
-                </Item>
-            </Menu>
-
-            <div className="loginBox">
-                <Input
-                    onChange={(e) => {accountInput(e.target.value)}}
-                    className={"input"}
-                    prefix={<UserOutlined className={'icon'} />}
-                    placeholder="登录账号"
-                />
-                <Input
-                    onChange={(e) => {passwordInput(e.target.value)}}
-                    className={"input"}
-                    prefix={<LockOutlined className={'icon'} />}
-                    placeholder="密码"
-                    type="password"
-                />
-            </div>
-
-            <div className="buttonBox">
-                <Button 
-                    type="primary" 
-                    className="loginBtn"
-                    onClick={() => {loginLocal()}}
-                >
-                    登录
-                </Button>
-            </div>
-        </StyleLoginBody>
+            </StyleLoginBody>
+        </>
     )
 }
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -1,13 +1,35 @@
 import Style from 'styled-components'
 
+export const StyleImageBox = Style.div`
+    width: 100%;
+    z-index: -114514;
+    position: absolute;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+`
+
 export const StyleLoginBody = Style.div`
     width: 100%;
     height: 100%;
-    background-color: #f5f5f5;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    .allBox {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        padding-top: 36px;
+        padding-bottom: 36px;
+        padding-left: 48px;
+        padding-right: 48px;
+        border-radius: 8px;
+        background-color: rgba(240,240,240, 0.5)
+    }
 
     .titleBox {
         display: flex;
@@ -38,7 +60,7 @@ export const StyleLoginBody = Style.div`
         display: flex;
         justify-content: center;
         width: 300px;
-        background-color: #f5f5f5;
+        background-color: #f0f0f0;
         margin-top: 36px;
     }
 
