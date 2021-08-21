9c618f3ab97aaedace8a02e0e2901378ae982408
RuntuS <893119806@qq.com>
2021-08-21 20:10:33 +0800

    hot-fix: request url

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
-let requestUrl;
+let requestUrl = 'http://36.133.57.158';
 
-// 开发环境
-if(process.env.NODE_ENV === 'development'){
-    requestUrl = 'http://36.133.57.158'
-} else {
-    requestUrl = 'http://localhost'
-}
 
 export const Axios = axios.create({
     baseURL: `${requestUrl}:8081/`
