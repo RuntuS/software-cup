33b101977a11cdfd8500b83f4fa4b7e3a1ba1ef1
RuntuS <893119806@qq.com>
2021-08-21 19:47:16 +0800

    feat: set env-variables to config request url

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -1,5 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
+
+let requestUrl;
+
+// 开发环境
+if(process.env.NODE_ENV === 'development'){
+    requestUrl = 'http://36.133.57.158'
+} else {
+    requestUrl = 'http://127.0.0.1'
+}
 
 export const Axios = axios.create({
-    baseURL: 'http://36.133.57.158:8081/'
+    baseURL: `${requestUrl}:8081/`
 })
