3f48a0039ca4e507950a4e0c09ce360ea4c96007
RuntuS <893119806@qq.com>
2021-08-21 19:55:14 +0800

    fix: fix request url

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -6,7 +6,7 @@ let requestUrl;
 if(process.env.NODE_ENV === 'development'){
     requestUrl = 'http://36.133.57.158'
 } else {
-    requestUrl = 'http://127.0.0.1'
+    requestUrl = 'http://localhost'
 }
 
 export const Axios = axios.create({
